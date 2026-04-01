3634395a4defe74c704f861dc56f1d91aaf574ca
Eske555 <nachoeskenazi@gmail.com>
2026-04-01 19:25:51 -0300

    Falta detalles gris y boton eliminar

--- mi-proyecto/src/App.jsx
+++ mi-proyecto/src/App.jsx
@@ -1,32 +1,19 @@
-import { useState } from "react";
-import Formulario from "./components/Formulario";
-import Listado from "./components/Listado";
-import Header from "./components/Header";
-import "./App.css";
+import Header from "./Componentes/Header";
+import Formulario from "./Componentes/Formulario";
+import ListaCita from "./Componentes/ListaCita";
 
 function App() {
-  const [citas, setCitas] = useState([
-    { id: 1, mascota: "Nina", dueño: "Martin", fecha: "2021-08-05", hora: "08:20", sintomas: "Le duele la pioma" },
-    { id: 2, mascota: "Sifon", dueño: "Flocha", fecha: "2023-08-05", hora: "09:21", sintomas: "Duerme mucho" },
-    { id: 3, mascota: "Floki", dueño: "Ari", fecha: "2023-09-05", hora: "16:15", sintomas: "" }
-  ]);
-
-  const agregarCita = (cita) => setCitas([...citas, { ...cita, id: Date.now() }]);
-  const eliminarCita = (id) => setCitas(citas.filter((c) => c.id !== id));
-
   return (
-    <div>
-      <h1>Administrador de Pacientes</h1>
+    <>
       <Header />
-      <div className="contenido">
-        <div className="formulario-col">
-          <Formulario agregarCita={agregarCita} />
-        </div>
-        <div className="listado-col">
-          <Listado citas={citas} eliminarCita={eliminarCita} />
+
+      <div className="container">
+        <div className="row">
+          <Formulario />
+          <ListaCita />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
